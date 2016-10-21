afae0f98f7424ee1cf31599a046ee83ee3ec4407
Dan <danm@tillercorp.com>
2016-10-21 15:35:48 -0500

    Added Key bindings for delete and add. Added command for delete and add.

--- MvvmDataBinding/ViewModels/OrganizationViewModel.cs
+++ MvvmDataBinding/ViewModels/OrganizationViewModel.cs
@@ -9,7 +9,7 @@ using Template10.Mvvm;
 
 namespace MvvmDataBinding.ViewModels
 {
-    public class OrganizationViewModel
+    public class OrganizationViewModel : ViewModelBase
     {
         private FakeService context = new FakeService();
         private ObservableCollection<Person> people = new ObservableCollection<Person>();
@@ -17,9 +17,12 @@ namespace MvvmDataBinding.ViewModels
         {
             people = new ObservableCollection<Person>(context.GetPeopleAsync().Result);
             DeleteCommand = new DelegateCommand(OnDelete, CanDelete);
+            AddCommand = new DelegateCommand(OnAdd, CanAdd);
         }
 
         public DelegateCommand DeleteCommand { get; private set; }
+
+        public DelegateCommand AddCommand { get; private set; }
 
         public ObservableCollection<Person> People
         {
@@ -28,13 +31,38 @@ namespace MvvmDataBinding.ViewModels
         }
 
         // Need selected list item code here....
+        private Person selectedPerson;
+
+        public Person SelectedPerson
+        {
+            get { return selectedPerson; }
+            set
+            {
+                selectedPerson = value;
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
+
 
         private bool CanDelete()
+        {
+            return SelectedPerson != null;
+        }
+
+        private async void OnDelete()
+        {
+            Person person = SelectedPerson;
+            people.Remove(person);
+            await context.DeletePersonAsync(person);
+        }
+
+
+        private bool CanAdd()
         {
             return true;
         }
 
-        private void OnDelete()
+        private void OnAdd()
         {
             
         }
